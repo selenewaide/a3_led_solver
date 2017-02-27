6bbf0b1200011be212529d9d2a361f03a857885b
Selene Waide <selene.waide@ucdconnect.ie>
2017-02-27 14:53:17 +0000

    led grid

--- led_solver/main_led.py
+++ led_solver/main_led.py
@@ -7,9 +7,39 @@ import os
 def check_file_exists(file_name):
     return os.path.isfile(file_name)
 
+# open files and read it into 'lines'
 def open_file_and_read(file_name):
     with open(file_name) as f:
         lines = f.read().splitlines()
     return lines
+
+# create a grid - two dimensional list
+def create_grid(grid_dimension):
+    led_grid = [[False]*grid_dimension for _ in range(grid_dimension)]
+    return led_grid
     
 
+def parse_commands(each_line):
+    
+    command = ""
+    params = ""
+    
+    if each_line.startswith("switch"):
+        command = "switch"
+        params = each_line.replace(command,"")
+        params = params.strip()
+    elif each_line.startswith("turn on"):
+        command = "turn on"
+        params = each_line.replace(command,"")
+        params = params.strip()
+    elif each_line.startswith("turn off"):
+        command = "turn off"
+        params = each_line.replace(command,"")
+        params = params.strip()
+        
+    return command, params
+    
+def shlug(name):
+    if name is None:
+        return False
+    return name.endswith('Shlug')
--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,10 @@
 '''
 
 # test.py
+import pprint
 import sys
 from nose.tools import *
-from led_solver.main_led import check_file_exists, open_file_and_read
+from led_solver.main_led import check_file_exists, open_file_and_read, create_grid, parse_commands, shlug
 
 def test_fileExists_validfile():
     ok_(check_file_exists("source_files/input_assign3_a.txt"), "This is an invalid path or file name.")
@@ -19,4 +20,19 @@ def test_open_file_and_read():
     eq_("5000",lines[0],"This is not a valid output.")
     eq_("turn off 3990,2730 through 4692,4333",lines[1],"This is not a valid output.")
     eq_("turn on 4511,2123 through 4512,2125",lines[-1],"This is not a valid output.")
+
+def test_create_grid():
+    test_grid = create_grid(10)
+    eq_(10,len(test_grid),"Error, this is not the size of the grid.")
+    #pprint.pprint(test_grid) -> prints the grid
+
+def test_parse_commands():
+    myString = parse_commands("turn off this is my str")
+    print(myString)
     
+def test_shlug():
+    ok_(shlug('RoShlug'))
+    ok_(shlug('SeleneShlug'))
+    
+def test_shlug_none():
+    ok_(not shlug(None))
